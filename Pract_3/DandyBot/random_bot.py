63b86f1379f1c4f051713c6288790124f5fe7b2a
CyberGeo335 <111456878+CyberGeo335@users.noreply.github.com>
2023-03-03 16:35:21 +0300

    Add files via upload
    Изменения в файлах:
    
    user_bot.py,random_bot.py
    
    (я ботов отключил, это слегка неправильно)

---
+++ Pract_3/DandyBot/random_bot.py
@@ -0,0 +1,7 @@
+﻿import random
+
+
+def script(check, x, y):
+    if not(check("gold", x, y)):
+        return "up"
+    return random.choice(["pass", "left", "right", "up", "down"])
---
+++ Pract_3/DandyBot/user_bot.py
@@ -0,0 +1,24 @@
+﻿def script(check, x, y):
+    if check("level") == 1:
+        if not (check("gold", x + 1, y)) and not (check("wall", x + 2, y)):
+            return "right"
+        elif check("gold", x + 1, y) and not (check("gold", x, y)):
+            return "right"
+        elif check("gold", x, y + 1) and not (check("gold", x, y)):
+            return "down"
+        elif check("gold", x, y):
+            return "take"
+
+    if check("level") == 2:
+        if check("wall", x - 1, y):
+            return "right"
+
+        if not(check("gold", x, y)):
+            return "up"
+
+        if check("gold", x, y):
+            return "take"
+
+        if not(check("empty", x + 1, y)):
+            return "right"
+
